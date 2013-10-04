8afc306a57936c913acb744ba745dee141f1c46d
Won-Kyu Park <wkpark@gmail.com>
2013-10-04 19:53:24 +0900

    update from moniwiki 1.2.1

--- header.php
+++ header.php
@@ -1,6 +1,33 @@
 <?php /* MoniWiki Theme by wkpark at kldp.org */
 if (!empty($DBInfo->use_scrap))
   include_once("plugin/scrap.php");
+if (!empty($this->_no_urlicons))
+  echo <<<EOF
+<style type='text/css'>
+img.url { /* display: none; /* */ }
+
+a.externalLink.unnamed {
+  background: url($this->themeurl/imgs/http.png) no-repeat 0 center;
+  padding: 0 0 0 14px;
+  opacity: .8;
+  filter: alpha(opacity=80);
+}
+
+a.externalLink.unnamed[target="_blank"]:after {
+  content: "";
+  background: url($this->themeurl/imgs/external.png) no-repeat 0 center;
+  display: inline-block;
+  width: 14px;
+  height: 14px;
+  vertical-align: middle;
+  margin: -2px 0 0 1px;
+  opacity: .7;
+  filter: alpha(opacity=70);
+}
+
+img.externalLink { display: none; }
+</style>\n
+EOF;
 ?>
 <div id='mainBody'>
 <div id='wikiIcon'><?php echo $upper_icon.$icons.$rss_icon?></div>
@@ -17,17 +44,20 @@ if (!empty($DBInfo->use_scrap))
 <?php echo $menu?>
 <?php empty($msg) ? '' : print $msg; ?>
 <div id='container'>
-<!-- ?php echo '<div id="wikiTrailer"><p><span>'.$trail.'</span></p></div>';?i -->
+<!-- ?php echo '<div id="wikiTrailer"><p><span>'.$trail.'</span></p></div>';? -->
 <div id='mycontent'>
 <?php echo '<div class="wikiTitle" id="wikiTitle">'.$title.'</div>';?>
 <?php echo $subindex;?>
 <?php
 if (empty($options['action']) and !empty($DBInfo->use_scrap)) {
-  $scrap = macro_Scrap($this);
+  $scrap = macro_Scrap($this, 'js');
   if (!empty($scrap)) {
-    echo "<div class='scrap'>";
+    echo "<div id='scrap'>";
     echo $scrap;
     echo "</div>";
+    $js = $this->get_javascripts();
+    if ($js) {
+      echo $js;
+    }
   }
 }
-?>
